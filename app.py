01d26636392cf1b09736ee72e11f381186d70434
ElJoker63 <eljoker630@gmail.com>
2024-11-20 00:53:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, send_file, abort, request
 app = Flask(__name__)
 
 # Configuración
-UPLOAD_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
+UPLOAD_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), './app/files')
 
 # Ruta principal
 @app.route('/')
